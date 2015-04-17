60fcbe71bf202833abfd9a7c29544af2dd6f724f
xy2189 <xinhaoyuan@gmail.com>
2015-04-17 00:06:53 -0400

    .

--- emacsconf/.config/emacs/_spell.el
+++ emacsconf/.config/emacs/_spell.el
@@ -1,3 +1,5 @@
 (setq ispell-list-command "--list")
-(setq ispell-program-name "/usr/local/bin/aspell")
+(setq ispell-program-name
+      (replace-regexp-in-string
+       "\r?\n$" "" (shell-command-to-string "command -v aspell")))
 (global-set-key (kbd "C-c ?") 'flyspell-correct-word-before-point)
