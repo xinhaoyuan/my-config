544d89510324985665f5e476ba01b5ac9d23ee56
Xinhao Yuan <xinhaoyuan@gmail.com>
2016-12-15 18:18:47 -0500

    .

---
+++ misc-linux/.config/awesome/my-widgets.lua
@@ -0,0 +1,98 @@
+local aw = require("awful")
+local be = require("beautiful")
+local wi = require("wibox")
+local sc = require("my-ui-scale")
+
+-- reserve room for conky
+
+dummy_status_bar = aw.wibox({ position = "top", screen = 1, ontop = false, width = 1, height = 20 * sc.factor })
+
+local my_wibox = {}
+local my_tag_list = {}
+local my_task_list = {}
+local my_tray = wi.widget.systray()
+
+my_tray:set_base_size(20 * sc.factor)
+
+my_tag_list.buttons = aw.util.table.join(
+   aw.button({ }, 1, aw.tag.viewonly),
+   aw.button({ "Mod4" }, 1, aw.client.movetotag),
+   aw.button({ }, 3, aw.tag.viewtoggle),
+   aw.button({ "Mod4" }, 3, aw.client.toggletag),
+   aw.button({ }, 4, function(t) aw.tag.viewnext(aw.tag.getscreen(t)) end),
+   aw.button({ }, 5, function(t) aw.tag.viewprev(aw.tag.getscreen(t)) end)
+)
+
+my_task_list.buttons = aw.util.table.join(
+   aw.button({ }, 1, function (c)
+         if c == client.focus then
+            c.minimized = true
+         else
+            -- Without this, the following
+            -- :isvisible() makes no sense
+            c.minimized = false
+            if not c:isvisible() then
+               aw.tag.viewonly(c:tags()[1])
+            end
+            -- This will also un-minimize
+            -- the client, if needed
+            client.focus = c
+            c:raise()
+         end
+   end),
+   aw.button({ }, 3, function ()
+         if instance then
+            instance:hide()
+            instance = nil
+         else
+            instance = aw.menu.clients({ width=500 })
+         end
+   end),
+   aw.button({ }, 4, function ()
+         aw.client.focus.byidx(1)
+         if client.focus then client.focus:raise() end
+   end),
+   aw.button({ }, 5, function ()
+         aw.client.focus.byidx(-1)
+         if client.focus then client.focus:raise() end
+end))
+
+for s = 1, screen.count() do
+   my_task_list[s] = aw.widget.tasklist.new(
+      s,
+      aw.widget.tasklist.filter.currenttags,
+      my_task_list.buttons,
+      {
+         font = "Sans " .. (10 * sc.factor)
+      }
+   )
+
+   my_tag_list[s] = aw.widget.taglist(
+      s, aw.widget.taglist.filter.all, my_tag_list.buttons,
+      {
+         font = "Sans " .. (10 * sc.factor)
+      }
+   )
+   
+   my_wibox[s] = aw.wibox({
+         screen = s,
+         fg = be.fg_normal,
+         bg = be.bg_normal,
+         height = 20 * sc.factor,
+         position = "bottom",
+         border_width = 0,
+   })
+
+   local left_layout = wi.layout.fixed.horizontal()
+   left_layout:add(my_tag_list[s])
+
+   local right_layout = wi.layout.fixed.horizontal()
+   right_layout:add(my_tray)
+
+   local layout = wi.layout.align.horizontal()
+   layout:set_left(left_layout)
+   layout:set_middle(my_task_list[s])
+   layout:set_right(right_layout)
+   
+   my_wibox[s]:set_widget(layout)
+end
--- misc-linux/.config/awesome/rc.lua
+++ misc-linux/.config/awesome/rc.lua
@@ -1,10 +1,10 @@
-require("my-utils")
 local aw = require("awful")
 local ar = require("awful.rules")
 local na = require("naughty")
 local be = require("beautiful")
 local wi = require("wibox")
 -- 3rd party libs
+local ut = require("my-utils")
 local af = require("my-autofocus")
 local sc = require("my-ui-scale")
 local cf = require("cyclefocus")
@@ -29,6 +29,7 @@ aw.util.spawn_with_shell("$HOME/.xdesktoprc.awesome")
 be.init("/usr/share/awesome/themes/default/theme.lua")
 
 -- layouts
+
 local layouts = {
    aw.layout.suit.tile,
    aw.layout.suit.fair,
@@ -40,101 +41,7 @@ for s = 1, screen.count() do
    tags[s] = aw.tag({ 1, 2 ,3, 4 }, s, layouts[1])
 end
 
--- reserve room for conky
-
-dummy_status_bar = aw.wibox({ position = "top", screen = 1, ontop = false, width = 1, height = 20 * sc.factor })
-
--- and bottom wibox for task list and tray
-
-local my_wibox = {}
-local my_tag_list = {}
-local my_task_list = {}
-local my_tray = wi.widget.systray()
-
-my_tray:set_base_size(20 * sc.factor)
-
-my_tag_list.buttons = aw.util.table.join(
-   aw.button({ }, 1, aw.tag.viewonly),
-   aw.button({ "Mod4" }, 1, aw.client.movetotag),
-   aw.button({ }, 3, aw.tag.viewtoggle),
-   aw.button({ "Mod4" }, 3, aw.client.toggletag),
-   aw.button({ }, 4, function(t) aw.tag.viewnext(aw.tag.getscreen(t)) end),
-   aw.button({ }, 5, function(t) aw.tag.viewprev(aw.tag.getscreen(t)) end)
-)
-
-my_task_list.buttons = aw.util.table.join(
-   aw.button({ }, 1, function (c)
-         if c == client.focus then
-            c.minimized = true
-         else
-            -- Without this, the following
-            -- :isvisible() makes no sense
-            c.minimized = false
-            if not c:isvisible() then
-               aw.tag.viewonly(c:tags()[1])
-            end
-            -- This will also un-minimize
-            -- the client, if needed
-            client.focus = c
-            c:raise()
-         end
-   end),
-   aw.button({ }, 3, function ()
-         if instance then
-            instance:hide()
-            instance = nil
-         else
-            instance = aw.menu.clients({ width=500 })
-         end
-   end),
-   aw.button({ }, 4, function ()
-         aw.client.focus.byidx(1)
-         if client.focus then client.focus:raise() end
-   end),
-   aw.button({ }, 5, function ()
-         aw.client.focus.byidx(-1)
-         if client.focus then client.focus:raise() end
-end))
-
-for s = 1, screen.count() do
-   my_task_list[s] = aw.widget.tasklist.new(
-      s,
-      aw.widget.tasklist.filter.currenttags,
-      my_task_list.buttons,
-      {
-         font = "Sans " .. (10 * sc.factor)
-      }
-   )
-
-   my_tag_list[s] = aw.widget.taglist(
-      s, aw.widget.taglist.filter.all, my_tag_list.buttons,
-      {
-         font = "Sans " .. (10 * sc.factor)
-      }
-   )
-   
-   my_wibox[s] = aw.wibox({
-         screen = s,
-         fg = be.fg_normal,
-         bg = be.bg_normal,
-         height = 20 * sc.factor,
-         position = "bottom",
-         border_width = 0,
-   })
-
-   local left_layout = wi.layout.fixed.horizontal()
-   left_layout:add(my_tag_list[s])
-
-   local right_layout = wi.layout.fixed.horizontal()
-   right_layout:add(my_tray)
-
-   local layout = wi.layout.align.horizontal()
-   layout:set_left(left_layout)
-   layout:set_middle(my_task_list[s])
-   layout:set_right(right_layout)
-   
-   my_wibox[s]:set_widget(layout)
-end
+require("my-widgets")
 
 -- helper functions
 
@@ -153,6 +60,18 @@ af.find_alternative_focus = function(c)
                and is_floating(nc) == f
          end
    })
+
+   if new_focus then return new_focus end
+
+   new_focus = cf.find_history(
+      0, {
+         function (nc)
+            return
+               nc ~= c
+               and cf.filters.same_screen(nc, c)
+         end
+   })
+
    return new_focus
 end
 
