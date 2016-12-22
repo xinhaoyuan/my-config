72080f2c837da835c6a5b601d7f3ec8cdd21b189
Xinhao Yuan <xinhaoyuan@gmail.com>
2016-12-22 03:14:34 -0500

    .

--- emacsconf/.config/emacs/_boogie.el
+++ emacsconf/.config/emacs/_boogie.el
@@ -1,6 +1,11 @@
 (if (require 'dafny-mode nil 'noerror)
     (progn
       (setq dafny-verification-backend 'server)
+      ;; remove the "prettified symbol" by compose-region
+      (let ((p (assoc "\\(\\_<forall\\_>\\).*?::"
+                      dafny-font-lock-keywords)))
+        (setcdr p (cdr (cdr p)))
+        )
       (add-hook 'dafny-mode-hook
                 (lambda ()
                   (setq-local tab-width 4)
@@ -9,6 +14,7 @@
                   (electric-indent-local-mode -1)
                   (fset 'boogie-friends-self-insert-and-indent 'self-insert-command)
                   (modify-syntax-entry ?_ "_")
+                  (prettify-symbols-mode -1)
                   ))
       (setq flycheck-dafny-executable
 	    (concat (getenv "HOME") "/opt/dafny/Dafny.exe"))
