5fa91f38240b577444a5e3034a16d0e8da5d511a
Xinhao Yuan <xinhaoyuan@gmail.com>
2017-11-10 13:15:41 -0500

    .

--- setup_dep.sh
+++ setup_dep.sh
@@ -3,8 +3,12 @@
 # this script needs root permission
 
 # basic dependency
-apt-get install stow tmux git zsh emacs build-essential consolekit xdotool xclip wmctrl xorg-dev php5-cli pkg-config
+apt-get install -y stow tmux git zsh emacs build-essential consolekit xdotool xclip wmctrl xorg-dev php5-cli pkg-config
 
 # dependency for building awesome
+apt-get install -y liblua5.3-dev lua-lgi libgdk-pixbuf-2.0-dev libxcb-xrm-dev
 # this is extracted from .travis.yml from awesome-git
-sudo apt-get install -y libcairo2-dev gir1.2-gtk-3.0 xmlto asciidoc libpango1.0-dev libxcb-xtest0-dev libxcb-icccm4-dev libxcb-randr0-dev libxcb-keysyms1-dev libxcb-xinerama0-dev libdbus-1-dev libxdg-basedir-dev libstartup-notification0-dev imagemagick libxcb1-dev libxcb-shape0-dev libxcb-util0-dev libx11-xcb-dev libxcb-cursor-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev
+apt-get install -y \
+        libcairo2-dev libxdg-basedir-dev libstartup-notification0-dev imagemagick \
+        gir1.2-gtk-3.0 libdbus-1-dev libpango1.0-dev libxkbcommon-dev libxkbcommon-x11-dev \
+        libxcb1-dev libx11-xcb-dev libxcb-xtest0-dev libxcb-icccm4-dev libxcb-randr0-dev libxcb-keysyms1-dev libxcb-xinerama0-dev libxcb-shape0-dev libxcb-util0-dev libxcb-cursor-dev libxcb-xkb-dev
