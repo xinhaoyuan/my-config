d85f358d01dfb370ce9f7f22e60bf4b0daa1c550
Xinhao Yuan <xinhaoyuan@gmail.com>
2017-12-17 00:17:30 -0500

    .

--- font_vfix.py
+++ font_vfix.py
@@ -9,22 +9,38 @@ def main():
     parser.add_argument("-d", "--adjust", dest = "adjust", action = "store",
                         type = int,
                         help = "do the fixing with delta")
+    parser.add_argument("-r", "--rename", dest = "rename", action = "store",
+                        type = str,
+                        help = "rename the font family name")
     parser.add_argument("fonts", metavar = "font", type = str, nargs = "+", help = "list of font files")
     args = parser.parse_args()
 
     base_dir = os.getcwd()
     for file_name in args.fonts:
-        if not re.match("consola.*\.ttf", file_name, flags = re.IGNORECASE):
+        if not re.match(".*\.ttf", file_name, flags = re.IGNORECASE):
             continue
         file_path = os.path.join(base_dir, file_name)
         try:
             font = ff.open(file_path)
             if args.adjust is not None:
                 sys.stdout.write("Working on \"{0}\": ".format(file_name))
+                if args.rename is not None:
+                    new_familyname = args.rename.format(name = font.familyname)
+                    new_fontname = font.fontname.replace(font.familyname.replace(" ", ""), new_familyname.replace(" ", "", 1))
+                    new_fullname = font.fullname.replace(font.familyname, new_familyname, 1)
+                    sys.stdout.write("Rename {0} => {1}\n".format(
+                        (font.fontname, font.familyname, font.fullname),
+                        (new_fontname, new_familyname, new_fullname)))
+                    font.fontname = new_fontname
+                    font.familyname = new_familyname
+                    font.fullname = new_fullname
                 font.hhea_ascent = font.hhea_ascent + args.adjust
                 font.hhea_descent = font.hhea_descent + args.adjust
                 font.os2_typoascent = font.os2_typoascent + args.adjust
                 font.os2_typodescent = font.os2_typodescent + args.adjust
+                # Is this really true?
+                font.os2_winascent = font.os2_winascent - args.adjust
+                font.os2_windescent = font.os2_windescent + args.adjust
                 font.ascent = font.ascent + args.adjust
                 font.descent = font.descent - args.adjust # this is negated
                 font.generate(file_path)
@@ -33,13 +49,14 @@ def main():
             else:
                 sys.stdout.write("Inspecting \"{0}\": ".format(file_name))
                 sys.stdout.write(
-                    "{0} {1} {2} {3} {4} {5}".format(
-                        font.hhea_ascent,
-                        font.hhea_descent,
-                        font.os2_typoascent,
-                        font.os2_typodescent,
-                        font.ascent,
-                        font.descent))
+                    "{0};{1};{2};{3}, {4}".format(
+                        (font.fontname, font.familyname, font.fullname),
+                        (font.hhea_ascent, font.hhea_descent),
+                        (font.os2_typoascent, font.os2_typodescent),
+                        (font.os2_winascent, font.os2_windescent),
+                        (font.ascent, font.descent)))
+        except Exception as x:
+            print("Got exception {0}".format(x))
         finally:
             sys.stdout.write("\n")
 
