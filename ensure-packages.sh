f9b78dcc5413fc74ad82b30bb6312f0a23b48770
Xinhao Yuan <xinhaoyuan@gmail.com>
2019-07-08 07:06:00 -0400

    .

--- ensure-packages.sh
+++ ensure-packages.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 sudo apt install stow zsh tmux emacs php-cli
-sudo apt install awesome awesome-extra compton rxvt-unicode-256color synapse rofi fcitx fcitx-pinyin nitrogen
+sudo apt install awesome awesome-extra compton rxvt-unicode-256color synapse rofi fcitx fcitx-pinyin nitrogen cbatticon
--- linux-x/bin/set_dpi.py
+++ linux-x/bin/set_dpi.py
@@ -7,31 +7,44 @@ import sys
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--dpi", type = int)
+parser.add_argument("--fallback-dpi", dest = "fallback_dpi", type = int)
+parser.add_argument("--max-dpi", dest = "max_dpi", type = int)
+parser.add_argument("--min-dpi", dest = "min_dpi", type = int)
 args = parser.parse_args()
 
-dpi = args.dpi
+dpi = None
+dpy_info = sp.check_output("xrandr | grep ' connected '", shell = True).decode("utf-8")
+for line in dpy_info.split('\n'):
+    if "primary" in line:
+        m = re.search("([0-9]+)mm x ([0-9]+)mm", line)
+        if not m:
+            continue
+        dev_width = int(m.group(1))
+        dev_height = int(m.group(2))
+
+        if dev_width == 0:
+            continue
+
+        m = re.search("([0-9]+)x([0-9]+)\\+[0-9]+\\+[0-9]+", line)
+        if not m:
+            continue
+        res_width = int(m.group(1))
+        res_height = int(m.group(2))
+
+        sys.stderr.write(
+            "Info: {} {} {} {}\n".format(dev_width, dev_height, res_width, res_height))
+
+        dpi_w = res_width / (dev_width / 25.4)
+        dpi = math.floor(dpi_w / 48) * 48
+
 if dpi is None:
-    dpy_info = sp.check_output("xrandr | grep ' connected '", shell = True).decode("utf-8")
-    for line in dpy_info.split('\n'):
-        if "primary" in line:
-            m = re.search("([0-9]+)mm x ([0-9]+)mm", line)
-            if not m:
-                continue
-            dev_width = int(m.group(1))
-            dev_height = int(m.group(2))
+    dpi = args.fallback_dpi
 
-            m = re.search("([0-9]+)x([0-9]+)\\+[0-9]+\\+[0-9]+", line)
-            if not m:
-                continue
-            res_width = int(m.group(1))
-            res_height = int(m.group(2))
+if dpi is not None and args.max_dpi is not None:
+    dpi = min(args.max_dpi, dpi)
 
-            sys.stderr.write(
-                "Info: {} {} {} {}\n".format(dev_width, dev_height, res_width, res_height))
-
-            dpi_w = res_width / (dev_width / 25.4)
-            dpi = math.floor(dpi_w / 48) * 48
+if dpi is not None and args.min_dpi is not None:
+    dpi = max(args.min_dpi, dpi)
 
 if dpi is not None:
     scaling = dpi / 96.0
