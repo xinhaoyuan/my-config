45f9f13e74648d84a619d4dc8e5c21817df18e00
Xinhao Yuan <xinhaoyuan@gmail.com>
2019-10-08 20:01:20 -0400

    .

--- linux-x/.config/awesome/config/screen.lua
+++ linux-x/.config/awesome/config/screen.lua
@@ -116,9 +116,9 @@ local my_tag_list_buttons = awful.util.table.join(
    awful.button({ }, 1, awful.tag.viewonly),
    awful.button({ "Mod4" }, 1, awful.client.movetotag),
    awful.button({ }, 3, awful.tag.viewtoggle),
-   awful.button({ "Mod4" }, 3, awful.client.toggletag)
-   -- awful.button({ }, 4, function(t) awful.tag.viewnext(awful.tag.getscreen(t)) end),
-   -- awful.button({ }, 5, function(t) awful.tag.viewprev(awful.tag.getscreen(t)) end)
+   awful.button({ "Mod4" }, 3, awful.client.toggletag),
+   awful.button({ }, 4, function(t) awful.tag.viewnext(awful.tag.getscreen(t)) end),
+   awful.button({ }, 5, function(t) awful.tag.viewprev(awful.tag.getscreen(t)) end)
 )
 
 local client_menu_selected = nil
