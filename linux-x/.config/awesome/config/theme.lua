dc1ab594ba693cd3ef206e5fadb86a6979162791
Xinhao Yuan <xinhaoyuan@gmail.com>
2019-11-17 21:37:51 -0500

    .

--- linux-x/.config/awesome/config/theme.lua
+++ linux-x/.config/awesome/config/theme.lua
@@ -1,9 +1,11 @@
 local capi = { mouse = mouse, client = client }
-local theme = dofile("/usr/share/awesome/themes/xresources/theme.lua")
+local theme_assets = require("beautiful.theme_assets")
 local awful = require("awful")
 local wibox = require("wibox")
 local beautiful = require("beautiful")
 local xresources = require("beautiful.xresources")
+local gfs = require("gears.filesystem")
+local themes_path = gfs.get_themes_dir()
 local dpi   = xresources.apply_dpi
 local xrdb  = xresources.get_current_theme()
 local gears = require("gears")
@@ -17,6 +19,84 @@ local cbg = require("contextual_background")
 local lgi   = require("lgi")
 local icons = require("icons")
 local cairo = lgi.cairo
+
+local c_black  = { xrdb.color0, xrdb.color8 }
+local c_red    = { xrdb.color1, xrdb.color9 }
+local c_green  = { xrdb.color2, xrdb.color10 }
+local c_yellow = { xrdb.color3, xrdb.color11 }
+local c_blue   = { xrdb.color4, xrdb.color12 }
+local c_purple = { xrdb.color5, xrdb.color13 }
+local c_cyan   = { xrdb.color6, xrdb.color14 }
+local c_white  = { xrdb.color7, xrdb.color15 }
+
+-- Copied from theme/xresource
+
+local theme = dofile(themes_path.."default/theme.lua")
+
+theme.bg_normal     = xrdb.background
+theme.bg_focus      = c_cyan[2]
+theme.bg_urgent     = c_red[2]
+theme.bg_minimize   = c_black[2]
+theme.bg_systray    = theme.bg_normal
+
+theme.fg_normal     = c_black[1]
+theme.fg_focus      = c_black[1]
+theme.fg_urgent     = theme.bg_normal
+theme.fg_minimize   = theme.bg_normal
+
+theme.useless_gap   = dpi(3)
+theme.border_width  = dpi(2)
+theme.border_normal = c_black[1]
+theme.border_focus  = c_cyan[2]
+theme.border_marked = c_green[2]
+
+-- There are other variable sets
+-- overriding the default one when
+-- defined, the sets are:
+-- taglist_[bg|fg]_[focus|urgent|occupied|empty|volatile]
+-- tasklist_[bg|fg]_[focus|urgent]
+-- titlebar_[bg|fg]_[normal|focus]
+-- tooltip_[font|opacity|fg_color|bg_color|border_width|border_color]
+-- mouse_finder_[color|timeout|animate_timeout|radius|factor]
+-- Example:
+--theme.taglist_bg_focus = "#ff0000"
+
+theme.tooltip_fg = theme.fg_normal
+theme.tooltip_bg = theme.bg_normal
+
+-- Variables set for theming the menu:
+-- menu_[bg|fg]_[normal|focus]
+-- menu_[border_color|border_width]
+theme.menu_submenu_icon = themes_path.."default/submenu.png"
+theme.menu_height = dpi(16)
+theme.menu_width  = dpi(100)
+
+-- You can add as many variables as
+-- you wish and access them by using
+-- beautiful.variable in your rc.lua
+--theme.bg_widget = "#cc0000"
+
+-- Recolor Layout icons:
+theme = theme_assets.recolor_layout(theme, theme.fg_normal)
+
+-- Define the icon theme for application icons. If not set then the icons
+-- from /usr/share/icons and /usr/share/icons/hicolor will be used.
+theme.icon_theme = nil
+
+-- Generate Awesome icon:
+theme.awesome_icon = theme_assets.awesome_icon(
+    theme.menu_height, theme.bg_focus, theme.fg_focus
+)
+
+-- Generate taglist squares:
+local taglist_square_size = dpi(4)
+theme.taglist_squares_sel = theme_assets.taglist_squares_sel(
+    taglist_square_size, theme.fg_normal
+)
+theme.taglist_squares_unsel = theme_assets.taglist_squares_unsel(
+    taglist_square_size, theme.fg_normal
+)
+
 
 theme.notification_icon_size = dpi(48)
 
