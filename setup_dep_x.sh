1def72ac79bd45d6d9cba7a178e117bd07f4bf73
Xinhao Yuan <xinhaoyuan@gmail.com>
2020-01-11 17:44:25 -0500

    .

--- setup_dep_x.sh
+++ setup_dep_x.sh
@@ -7,7 +7,7 @@ PKGS_AWESOME_SRC="libcairo2-dev libxdg-basedir-dev libstartup-notification0-dev 
 gir1.2-gtk-3.0 libdbus-1-dev libpango1.0-dev libxkbcommon-dev libxkbcommon-x11-dev \
 libxcb1-dev libx11-xcb-dev libxcb-xtest0-dev libxcb-icccm4-dev libxcb-randr0-dev libxcb-keysyms1-dev libxcb-xinerama0-dev libxcb-shape0-dev libxcb-util0-dev libxcb-cursor-dev libxcb-xkb-dev \
 libxcb-xfixes0-dev libxcb-xrm-dev libgdk-pixbuf2.0-dev \
-liblua5.3-dev lua5.3 lua-lgi-dev"
+liblua5.3-dev lua5.3 lua-lgi-dev librsvg-2-dev"
 
 sudo apt-get install -y $PKG_UTILS
 sudo apt-get install -y $PKG_AWESOME_SRC
