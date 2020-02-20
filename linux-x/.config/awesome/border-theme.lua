c7fbf24378029a152d11624868b9fe54bc10cb14
Xinhao Yuan <xinhaoyuan@gmail.com>
2020-02-19 23:31:08 -0500

    .

--- linux-x/.config/awesome/border-theme.lua
+++ linux-x/.config/awesome/border-theme.lua
@@ -129,6 +129,12 @@ function rounded_theme:init()
         self.size = beautiful.border_width
         self.outer_space = beautiful.border_outer_space
         self.inner_space = beautiful.border_inner_space
+        self.space_color = gcolor(beautiful.border_space)
+        self.outer_light_color = "#aaaaaa"
+        self.outer_dark_color = "#666666"
+        self.outer_light_gcolor = gcolor(self.outer_light_color)
+        self.outer_dark_gcolor = gcolor(self.outer_dark_color)
+        self.outer_transient_gcolor = gcolor("linear:0,0:"..tostring(self.size)..","..tostring(self.size)..":0,"..self.outer_light_color..":1,"..self.outer_dark_color)
         self.inited = true
     end
 end
@@ -136,13 +142,24 @@ end
 function rounded_theme:before_draw(context, cr)
     self:init()
     cr:save()
-    cr:set_source(gcolor(beautiful.border_space))
-    cr:paint()
-    cr:set_line_width(self.line_width)
-    cr:set_source(gcolor(context.color))
+end
+
+function rounded_theme:after_draw(context, cr)
+    cr:restore()
 end
 
 function rounded_theme:top_left(context, cr)
+    cr:rectangle(0, 0, self.size, self.size)
+    cr:set_source(self.outer_light_gcolor)
+    cr:fill()
+
+    cr:arc(self.size, self.size, (self.size - self.outer_space + self.inner_space) / 2, math.pi, math.pi * 1.5)
+    cr:line_to(self.size, self.size)
+    cr:close_path()
+    cr:set_source(self.space_color)
+    cr:fill()
+
+    cr:set_source(gcolor(context.color))
     cr:arc(self.size, self.size, self.size - self.outer_space, math.pi, math.pi * 1.5)
     cr:line_to(self.size, self.size - self.inner_space)
     cr:arc_negative(self.size, self.size, self.inner_space, math.pi * 1.5, math.pi)
@@ -151,6 +168,17 @@ function rounded_theme:top_left(context, cr)
 end
 
 function rounded_theme:top_right(context, cr)
+    cr:rectangle(0, 0, self.size, self.size)
+    cr:set_source(self.outer_transient_gcolor)
+    cr:fill()
+
+    cr:arc(0, self.size, (self.size - self.outer_space + self.inner_space) / 2, math.pi * -0.5, 0)
+    cr:line_to(0, self.size)
+    cr:close_path()
+    cr:set_source(self.space_color)
+    cr:fill()
+
+    cr:set_source(gcolor(context.color))
     cr:arc(0, self.size, self.size - self.outer_space, math.pi * -0.5, 0)
     cr:line_to(0, self.size)
     cr:arc_negative(0, self.size, self.inner_space, 0, math.pi * -0.5)
@@ -159,6 +187,17 @@ function rounded_theme:top_right(context, cr)
 end
 
 function rounded_theme:bottom_left(context, cr)
+    cr:rectangle(0, 0, self.size, self.size)
+    cr:set_source(self.outer_transient_gcolor)
+    cr:fill()
+
+    cr:arc(self.size, 0, (self.size - self.outer_space + self.inner_space) / 2, math.pi * 0.5, math.pi)
+    cr:line_to(self.size, 0)
+    cr:close_path()
+    cr:set_source(self.space_color)
+    cr:fill()
+
+    cr:set_source(gcolor(context.color))
     cr:arc(self.size, 0, self.size - self.outer_space, math.pi * 0.5, math.pi)
     cr:line_to(self.size, 0)
     cr:arc_negative(self.size, 0, self.inner_space, math.pi, math.pi * 0.5)
@@ -167,6 +206,17 @@ function rounded_theme:bottom_left(context, cr)
 end
 
 function rounded_theme:bottom_right(context, cr)
+    cr:rectangle(0, 0, self.size, self.size)
+    cr:set_source(self.outer_dark_gcolor)
+    cr:fill()
+
+    cr:arc(0, 0, (self.size - self.outer_space + self.inner_space) / 2, 0, math.pi * 0.5)
+    cr:line_to(0, 0)
+    cr:close_path()
+    cr:set_source(self.space_color)
+    cr:fill()
+
+    cr:set_source(gcolor(context.color))
     cr:arc(0, 0, self.size - self.outer_space, 0, math.pi * 0.5)
     cr:line_to(0, 0)
     cr:arc_negative(0, 0, self.inner_space, math.pi * 0.5, 0)
@@ -178,6 +228,15 @@ function rounded_theme:top(context, cr, length)
     if length <= 0 then
         return
     end
+
+    cr:rectangle(0, 0, length, self.size)
+    cr:set_source(self.outer_light_gcolor)
+    cr:fill()
+    cr:rectangle(0, (self.size - self.inner_space + self.outer_space) / 2, length, (self.size + self.inner_space - self.outer_space) / 2)
+    cr:set_source(self.space_color)
+    cr:fill()
+
+    cr:set_source(gcolor(context.color))
     cr:rectangle(0, self.outer_space, length, self.size - self.outer_space - self.inner_space)
     cr:fill()
 end
@@ -186,6 +245,15 @@ function rounded_theme:bottom(context, cr, length)
     if length <= 0 then
         return
     end
+
+    cr:rectangle(0, 0, length, self.size)
+    cr:set_source(self.outer_dark_gcolor)
+    cr:fill()
+    cr:rectangle(0, 0, length, (self.size + self.inner_space - self.outer_space) / 2)
+    cr:set_source(self.space_color)
+    cr:fill()
+
+    cr:set_source(gcolor(context.color))
     cr:rectangle(0, self.inner_space, length, self.size - self.outer_space - self.inner_space)
     cr:fill()
 end
@@ -194,6 +262,15 @@ function rounded_theme:left(context, cr, length)
     if length <= 0 then
         return
     end
+
+    cr:rectangle(0, 0, self.size, length)
+    cr:set_source(self.outer_light_gcolor)
+    cr:fill()
+    cr:rectangle((self.size - self.inner_space + self.outer_space) / 2, 0, (self.size + self.inner_space - self.outer_space) / 2, length)
+    cr:set_source(self.space_color)
+    cr:fill()
+
+    cr:set_source(gcolor(context.color))
     cr:rectangle(self.outer_space, 0, self.size - self.outer_space - self.inner_space, length)
     cr:fill()
 end
@@ -203,12 +280,17 @@ function rounded_theme:right(context, cr, length)
     if length <= 0 then
         return
     end
+
+    cr:rectangle(0, 0, self.size, length)
+    cr:set_source(self.outer_dark_gcolor)
+    cr:fill()
+    cr:rectangle(0, 0, (self.size + self.inner_space - self.outer_space) / 2, length)
+    cr:set_source(self.space_color)
+    cr:fill()
+
+    cr:set_source(gcolor(context.color))
     cr:rectangle(self.inner_space, 0, self.size - self.outer_space - self.inner_space, length)
     cr:fill()
-end
-
-function rounded_theme:after_draw(context, cr)
-    cr:restore()
 end
 
 mod.rounded_theme = rounded_theme
@@ -220,105 +302,105 @@ function mod:draw(context, cr, width, height, directions)
     theme:before_draw(context, cr)
     if directions == 15 then
         -- full box
-        theme:top_left(context, cr)
+        cr:save(); theme:top_left(context, cr); cr:restore()
         cr:translate(theme.size, 0)
-        theme:top(context, cr, width - theme.size * 2)
+        cr:save(); theme:top(context, cr, width - theme.size * 2); cr:restore()
         cr:translate(width - theme.size * 2, 0)
-        theme:top_right(context, cr)
+        cr:save(); theme:top_right(context, cr); cr:restore()
         cr:translate(0, theme.size)
-        theme:right(context, cr, height - theme.size * 2)
+        cr:save(); theme:right(context, cr, height - theme.size * 2); cr:restore()
         cr:translate(theme.size - width, 0)
-        theme:left(context, cr, height - theme.size * 2)
+        cr:save(); theme:left(context, cr, height - theme.size * 2); cr:restore()
         cr:translate(0, height - theme.size * 2)
-        theme:bottom_left(context, cr)
+        cr:save(); theme:bottom_left(context, cr); cr:restore()
         cr:translate(theme.size, 0)
-        theme:bottom(context, cr, width - theme.size * 2)
+        cr:save(); theme:bottom(context, cr, width - theme.size * 2); cr:restore()
         cr:translate(width - theme.size * 2, 0)
-        theme:bottom_right(context, cr)
+        cr:save(); theme:bottom_right(context, cr); cr:restore()
     elseif directions == 14 then
         -- no top
         cr:translate(width - theme.size, 0)
-        theme:right(context, cr, height - theme.size)
+        cr:save(); theme:right(context, cr, height - theme.size); cr:restore()
         cr:translate(theme.size - width, 0)
-        theme:left(context, cr, height - theme.size)
+        cr:save(); theme:left(context, cr, height - theme.size); cr:restore()
         cr:translate(0, height - theme.size)
-        theme:bottom_left(context, cr)
+        cr:save(); theme:bottom_left(context, cr); cr:restore()
         cr:translate(theme.size, 0)
-        theme:bottom(context, cr, width - theme.size * 2)
+        cr:save(); theme:bottom(context, cr, width - theme.size * 2); cr:restore()
         cr:translate(width - theme.size * 2, 0)
-        theme:bottom_right(context, cr)
+        cr:save(); theme:bottom_right(context, cr); cr:restore()
     elseif directions == 13 then
         -- no bottom
-        theme:top_left(context, cr)
+        cr:save(); theme:top_left(context, cr); cr:restore()
         cr:translate(theme.size, 0)
-        theme:top(context, cr, width - theme.size * 2)
+        cr:save(); theme:top(context, cr, width - theme.size * 2); cr:restore()
         cr:translate(width - theme.size * 2, 0)
-        theme:top_right(context, cr)
+        cr:save(); theme:top_right(context, cr); cr:restore()
         cr:translate(0, theme.size)
-        theme:right(context, cr, height - theme.size)
+        cr:save(); theme:right(context, cr, height - theme.size); cr:restore()
         cr:translate(theme.size - width, 0)
-        theme:left(context, cr, height - theme.size)
+        cr:save(); theme:left(context, cr, height - theme.size); cr:restore()
     elseif directions == 11 then
         -- no left
-        theme:top(context, cr, width - theme.size)
+        cr:save(); theme:top(context, cr, width - theme.size); cr:restore()
         cr:translate(width - theme.size, 0)
-        theme:top_right(context, cr)
+        cr:save(); theme:top_right(context, cr); cr:restore()
         cr:translate(0, theme.size)
-        theme:right(context, cr, height - theme.size * 2)
+        cr:save(); theme:right(context, cr, height - theme.size * 2); cr:restore()
         cr:translate(theme.size - width, height - theme.size * 2)
-        theme:bottom(context, cr, width - theme.size)
+        cr:save(); theme:bottom(context, cr, width - theme.size); cr:restore()
         cr:translate(width - theme.size, 0)
-        theme:bottom_right(context, cr)
+        cr:save(); theme:bottom_right(context, cr); cr:restore()
     elseif directions == 7 then
         -- no right
-        theme:top_left(context, cr)
+        cr:save(); theme:top_left(context, cr); cr:restore()
         cr:translate(theme.size, 0)
-        theme:top(context, cr, width - theme.size)
+        cr:save(); theme:top(context, cr, width - theme.size); cr:restore()
         cr:translate(-theme.size, theme.size)
-        theme:left(context, cr, height - theme.size * 2)
+        cr:save(); theme:left(context, cr, height - theme.size * 2); cr:restore()
         cr:translate(0, height - theme.size * 2)
-        theme:bottom_left(context, cr)
+        cr:save(); theme:bottom_left(context, cr); cr:restore()
         cr:translate(theme.size, 0)
-        theme:bottom(context, cr, width - theme.size)
+        cr:save(); theme:bottom(context, cr, width - theme.size); cr:restore()
     elseif directions == 5 then
         -- top + left
-        theme:top_left(context, cr)
+        cr:save(); theme:top_left(context, cr); cr:restore()
         cr:translate(theme.size, 0)
-        theme:top(context, cr, width - theme.size)
+        cr:save(); theme:top(context, cr, width - theme.size); cr:restore()
         cr:translate(-theme.size, theme.size)
-        theme:left(context, cr, height - theme.size)
+        cr:save(); theme:left(context, cr, height - theme.size); cr:restore()
     elseif directions == 9 then
         -- top + right
-        theme:top(context, cr, width - theme.size)
+        cr:save(); theme:top(context, cr, width - theme.size); cr:restore()
         cr:translate(width - theme.size, 0)
-        theme:top_right(context, cr)
+        cr:save(); theme:top_right(context, cr); cr:restore()
         cr:translate(0, theme.size)
-        theme:right(context, cr, height - theme.size)
+        cr:save(); theme:right(context, cr, height - theme.size); cr:restore()
     elseif directions == 6 then
         -- bottom + left
-        theme:left(context, cr, height - theme.size)
+        cr:save(); theme:left(context, cr, height - theme.size); cr:restore()
         cr:translate(0, height - theme.size)
-        theme:bottom_left(context, cr)
+        cr:save(); theme:bottom_left(context, cr); cr:restore()
         cr:translate(theme.size, 0)
-        theme:bottom(context, cr, width - theme.size)
+        cr:save(); theme:bottom(context, cr, width - theme.size); cr:restore()
     elseif directions == 10 then
         -- bottom + right
         cr:translate(width - theme.size, 0)
-        theme:right(context, cr, height - theme.size)
+        cr:save(); theme:right(context, cr, height - theme.size); cr:restore()
         cr:translate(0, height - theme.size)
-        theme:bottom_right(context, cr)
+        cr:save(); theme:bottom_right(context, cr); cr:restore()
         cr:translate(theme.size - width, 0)
-        theme:bottom(context, cr, width - theme.size)
+        cr:save(); theme:bottom(context, cr, width - theme.size); cr:restore()
     elseif directions == 1 then
-        theme:top(context, cr, width)
+        cr:save(); theme:top(context, cr, width); cr:restore()
     elseif directions == 2 then
-        theme:bottom(context, cr, width)
+        cr:save(); theme:bottom(context, cr, width); cr:restore()
     elseif directions == 4 then
-        theme:left(context, cr, height)
+        cr:save(); theme:left(context, cr, height); cr:restore()
     elseif directions == 8 then
-        theme:right(context, cr, height)
+        cr:save(); theme:right(context, cr, height); cr:restore()
     end
-    theme:after_draw(context, cr)
+    theme:after_draw(context, cr);
 end
 
 return mod
