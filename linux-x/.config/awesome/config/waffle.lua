e8b1f79547d1d4c2b48824141aa386a865202871
Xinhao Yuan <xinhaoyuan@gmail.com>
2021-06-28 13:12:36 -0400

    More way to switch clients during client waffle.

--- linux-x/.config/awesome/config/waffle.lua
+++ linux-x/.config/awesome/config/waffle.lua
@@ -30,6 +30,7 @@ local orgenda = require("orgenda")
 local mycalendar = require("my-calendar")
 local scroller = require("scroller")
 local notix = require("notix")
+local fts = require("hotpot").focus_timestamp
 local dpi = require("beautiful.xresources").apply_dpi
 
 local waffle_width = beautiful.waffle_panel_width or dpi(240)
@@ -2144,7 +2145,8 @@ local waffle_client_icon_container = wibox.widget {
 }
 
 local update_client_waffle_labels
-local client_waffle_focus = false
+local client_waffle_attached = false
+local client_waffle_transient = false
 local client_waffle = view {
     root = decorate_waffle {
         decorate_panel {
@@ -2326,12 +2328,20 @@ local client_waffle = view {
         spacing = button_padding,
         layout = wibox.layout.fixed.vertical,
     },
+    on_open = function()
+        if client_waffle_attached and not client_waffle_transient then
+            awful.client.focus.history.disable_tracking()
+        end
+        client_waffle_transient = false
+    end,
     on_close = function()
-        if client_waffle_focus then
+        if client_waffle_attached and not client_waffle_transient then
+            awful.client.focus.history.enable_tracking()
             local c = shared.waffle_selected_client
-            client_waffle_focus = false
+            client_waffle_attached = false
             gtimer.delayed_call(function () c:emit_signal("request::activate", "switch", {raise=true}) end)
         end
+        client_waffle_transient = false
         shared.waffle_selected_client = nil
     end,
     key_filter = function (mod, key, event)
@@ -2343,7 +2353,7 @@ local client_waffle = view {
     default_key_handler = function (mod, key, event)
         if event ~= "press" then return end
         if key == "Return" then
-            client_waffle_focus = true
+            client_waffle_attached = true
             waffle:hide()
             return
         end
@@ -2353,21 +2363,72 @@ local client_waffle = view {
         end
         for i = 1, #mod do mod[mod[i]] = true end
         if key == "Left" or key == "Right" or key == "Up" or key == "Down" then
-            if not client_waffle_focus then return end
-            if not mod.Mod1 then
-                awful.client.focus.global_bydirection(key:lower(), shared.waffle_selected_client)
-                local c = capi.client.focus
-                if c then
-                    client_waffle_focus = false
-                    waffle:hide()
+            if not client_waffle_attached then return end
 
-                    client_waffle_focus = true
-                    c:raise()
-                    capi.awesome.emit_signal("show_client_waffle", c, "client")
+            local c = shared.waffle_selected_client
+            if c.maximized then return end
+
+            -- This would only work single-threaded.
+            local old_visible = awful.client.visible
+            awful.client.visible = function (...)
+                local result = old_visible(...)
+                local head = 0
+                for i = 1, #result do
+                    if not result[i].maximized then
+                        head = head + 1
+                        result[head] = result[i]
+                    end
                 end
+                for i = #result, head + 1, -1 do
+                    result[i] = nil
+                end
+                return result
+            end
+            pcall(awful.client.focus.global_bydirection, key:lower(), c)
+            awful.client.visible = old_visible
+
+            c = capi.client.focus
+            if c then
+                client_waffle_transient = true
+                waffle:hide()
+
+                c:raise()
+                client_waffle_transient = true
+                capi.awesome.emit_signal("show_client_waffle", c, "client")
+            end
+            return
+        elseif key == "Prior" or key == "Next" then
+            if not client_waffle_attached then return end
+
+            local c = shared.waffle_selected_client
+            local clients = capi.client.get(c.screen)
+            table.sort(clients, function (a, b)
+                           return fts.get(a) > fts.get(b)
+                       end)
+            for i = 1, #clients do
+                if clients[i] == c then
+                    if key == "Prior" and i > 1 then
+                        c = clients[i - 1]
+                    elseif key == "Next" and i < #clients then
+                        c = clients[i + 1]
+                    else
+                        c = nil
+                    end
+                    break
+                end
+            end
+
+            if c then
+                client_waffle_transient = true
+                waffle:hide()
+
+                c:raise()
+                client_waffle_transient = true
+                capi.awesome.emit_signal("show_client_waffle", c, "client")
             end
             return
         end
+
     end,
 }
 
@@ -2376,7 +2437,7 @@ function shared.waffle.show_client_waffle(c, args)
     if args.anchor == "client" then
         local geo = c:geometry()
         args.anchor = { x = geo.x + geo.width / 2, y = geo.y + geo.height / 2 }
-        client_waffle_focus = true
+        client_waffle_attached = true
     end
     waffle:show(client_waffle, args)
     shared.waffle_selected_client = c
