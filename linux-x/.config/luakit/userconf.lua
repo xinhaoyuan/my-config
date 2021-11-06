814178aaeea0601ff76c198e59c6698422bdba87
Xinhao Yuan <xinhaoyuan@gmail.com>
2021-11-06 17:31:37 -0400

    Set ui-monospace alias.

--- linux-x/.config/luakit/userconf.lua
+++ linux-x/.config/luakit/userconf.lua
@@ -3,6 +3,7 @@ local window = require("window")
 local vertical_tab = require("vertical_tab")
 local tablist = require("lousy.widget.tablist")
 local settings = require("settings")
+local webview = require("webview")
 
 modes.add_binds(
     "normal", {
@@ -12,6 +13,22 @@ modes.add_binds(
     })
 
 tablist.min_width = 0
+
+local global_stylesheet = stylesheet{
+    source = [===[
+@font-face {
+  font-family: "ui-monospace";
+  src: local("monospace");
+}
+]===] }
+
+webview.add_signal(
+    "init", function (view)
+        view:add_signal(
+            "stylesheet", function (v)
+                v.stylesheets[global_stylesheet] = true;
+            end)
+    end)
 
 settings.set_setting("tablist.visibility", "always")
 settings.set_setting("webview.zoom_level", 150)
