481b452084ebf23aeef0fd7ba7838c7683b1adaf
Xinhao Yuan <xinhaoyuan@gmail.com>
2022-09-21 18:21:00 -0400

    Create wibox proxies to simplify wrapped widget creation.

--- linux-x/.config/awesome/prism/init.lua
+++ linux-x/.config/awesome/prism/init.lua
@@ -1,5 +1,20 @@
+local function get_proxy(origin_table, transform_func)
+    return setmetatable(
+        {}, {
+            __index = function(_self, key)
+                local v = origin_table[key]
+                if v == nil then return nil end
+                return transform_func(v)
+            end
+        })
+end
+
+local wrap = require(... .. ".wrap")
 return {
     picker = require(... .. ".picker"),
     layer = require(... .. ".layer"),
-    wrap = require(... .. ".wrap"),
+    wrap = wrap,
+    container = get_proxy(require("wibox.container"), wrap),
+    layout = get_proxy(require("wibox.layout"), wrap),
+    widget = get_proxy(require("wibox.widget"), wrap),
 }
